5d3ff321faea4584368275539e242b49524fe1cc
Djones106 <djones106@gmail.com>
2021-10-20 20:30:05 -0400

    Adding images

--- README.md
+++ README.md
@@ -1,2 +1,3 @@
 # fire-water-wind-earth-game
  A game to see if you can win against the elements
+Icon attribute <div>Icons made by <a href="https://www.flaticon.com/authors/eucalyp" title="Eucalyp">Eucalyp</a> from <a href="https://www.flaticon.com/" title="Flaticon">www.flaticon.com</a></div>
--- game.py
+++ game.py
@@ -1,22 +1,35 @@
 # Fire, Water, Wind, Earth Game
 import pygame
-from random import randint
+
 #iniating the game
 pygame.init()
 
 #creating game window
-screen_width = 800
-screen_height = int(screen_width * 0.8)
+screen = pygame.display.set_mode((800, 600))
+#Title and Icon 
+pygame.display.set_caption('Fire, Air, Eath, Water')
 
-screen = pygame.display.set_mode((screen_width, screen_height))
-pygame.display.set_caption('Fire Water Wind Earth')
+#Elements
+fireIcon = pygame.image.load('02_Fire_Element.png')
+fireX = 370
+fireY = 480
 
-#running the game
+def fire ():
+    screen.blit(fireIcon, fireX, fireY)
+
+#Game Loop
 run = True
 while run:
-
+    #RGB - Red, Green, Blue    
+    screen.fill((211,211,211))
     for event in pygame.event.get():
         #quit game
-        if event.type == pygame.QUIT:
+        if event.type == pygame.QUIT: #need to be in all caps
             run = False
+  
+    fire()
+
+pygame.display.update()    
+
+
 pygame.quit()
